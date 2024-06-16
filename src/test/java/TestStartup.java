605ec7f67589785947c1134f8c2bb4f70ab04969
FT-Fetters <1438255191@qq.com>
2024-06-16 21:31:30 +0800

    test(startup): add test case for web startup configuration

--- src/test/java/TestStartup.java
+++ src/test/java/TestStartup.java
@@ -1,4 +1,24 @@
+import com.heybcat.docker.pull.CommandStartup;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.locks.LockSupport;
+import org.junit.Test;
+
 public class TestStartup {
+
+    @Test
+    public void testWeb(){
+        try {
+            CommandStartup.main(new String[]{"--web"});
+            LockSupport.park();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
