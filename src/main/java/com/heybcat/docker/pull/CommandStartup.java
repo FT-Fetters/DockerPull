364ade70fdef6897aa5ec589999d7eba7790a485
FT-Fetters <1438255191@qq.com>
2024-06-16 21:29:43 +0800

    Added web startup

--- src/main/java/com/heybcat/docker/pull/CommandStartup.java
+++ src/main/java/com/heybcat/docker/pull/CommandStartup.java
@@ -1,6 +1,7 @@
 package com.heybcat.docker.pull;
 
 import com.heybcat.docker.pull.core.DockerPull;
+import com.heybcat.docker.pull.web.DockerPullWebApplication;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.logging.Logger;
@@ -10,15 +11,40 @@ import java.util.logging.Logger;
  */
 public class CommandStartup {
 
+    private static final String WEB_FLAG = "--web";
     static Logger log = Logger.getLogger(CommandStartup.class.getName());
 
     public static void main(String[] args)
         throws URISyntaxException, IOException, InterruptedException {
-        if (args.length == 0){
-            log.info("Usage: java -jar docker-pull.jar <image> <proxyUrl> <proxyPort>");
+        handleCommand(args);
+    }
+
+    private static void handleCommand(String[] args)
+        throws URISyntaxException, IOException, InterruptedException {
+        if (args.length == 0) {
+            logUsage();
             return;
         }
-        DockerPull.pull(args[0], args[1], Integer.parseInt(args[2]));
+
+        if (args.length == 3) {
+            DockerPull.pull(args[0], args[1], Integer.parseInt(args[2]));
+            return;
+        }
+
+        if (args.length == 1) {
+            if (WEB_FLAG.equals(args[0])) {
+                DockerPullWebApplication.run();
+            } else {
+                DockerPull.pull(args[0], null, null);
+            }
+            return;
+        }
+        logUsage();
+    }
+
+    private static void logUsage() {
+        log.info("Usage: java -jar docker-pull.jar <image> <proxyUrl> <proxyPort>");
+        log.info("       java -jar docker-pull.jar --web");
     }
 
 }
