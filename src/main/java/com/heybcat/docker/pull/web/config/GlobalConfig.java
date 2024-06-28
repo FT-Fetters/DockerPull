65ccfa047119c3eed01776dd2888618ef7e76159
FT-Fetters <1438255191@qq.com>
2024-06-28 11:47:03 +0800

    feat(GlobalConfig): add SSH configuration management
    Extend the GlobalConfig class to include SSH connection settings such as host, port, user, password, and save path. This enhancement allows for configuring SSH connections dynamically at runtime through the use of the Config annotation and corresponding getters and setters.

--- src/main/java/com/heybcat/docker/pull/web/config/GlobalConfig.java
+++ src/main/java/com/heybcat/docker/pull/web/config/GlobalConfig.java
@@ -5,9 +5,26 @@ package com.heybcat.docker.pull.web.config;
  */
 public class GlobalConfig {
 
+    @Config("proxyHost")
     private static String proxyHost;
 
+    @Config("proxyPort")
     private static String proxyPort;
+
+    @Config("sshHost")
+    private static String sshHost;
+
+    @Config("sshPort")
+    private static String sshPort;
+
+    @Config("sshUser")
+    private static String sshUser;
+
+    @Config("sshPassword")
+    private static String sshPassword;
+
+    @Config("sshSavePath")
+    private static String sshSavePath;
 
     public static String getProxyHost() {
         return proxyHost;
@@ -23,5 +40,45 @@ public class GlobalConfig {
 
     public static void setProxyPort(String proxyPort) {
         GlobalConfig.proxyPort = proxyPort;
+    }
+
+    public static String getSshHost() {
+        return sshHost;
+    }
+
+    public static void setSshHost(String sshHost) {
+        GlobalConfig.sshHost = sshHost;
+    }
+
+    public static String getSshPort() {
+        return sshPort;
+    }
+
+    public static void setSshPort(String sshPort) {
+        GlobalConfig.sshPort = sshPort;
+    }
+
+    public static String getSshUser() {
+        return sshUser;
+    }
+
+    public static void setSshUser(String sshUser) {
+        GlobalConfig.sshUser = sshUser;
+    }
+
+    public static String getSshPassword() {
+        return sshPassword;
+    }
+
+    public static void setSshPassword(String sshPassword) {
+        GlobalConfig.sshPassword = sshPassword;
+    }
+
+    public static String getSshSavePath() {
+        return sshSavePath;
+    }
+
+    public static void setSshSavePath(String sshSavePath) {
+        GlobalConfig.sshSavePath = sshSavePath;
     }
 }
